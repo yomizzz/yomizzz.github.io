adcedf249550fb159e1dfde3b92fd88c01467ae7
yomizzz <zhaiyaomin1818@gmail.com>
2021-08-15 15:14:39 +0800

    modified the pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = 'yomizzz'
+AUTHOR = 'yomi'
 SITENAME = 'Yomi blog'
+SITESUBTITLE = 'A personal blog.'
 SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'Asia/Shanghai'
 
-DEFAULT_LANG = 'Chinese (Simplified)'
+DEFAULT_LANG = 'cn'
+
+GITHUB_URL = 'https://github.com/yomizzz/'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,16 +22,21 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Notion', 'https://y0m1.notion.site/yomi-aa1b6d79b81b4319b48dd6d465f95c4c'),
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://github.com/yomizzz/'),
+          )
 
 DEFAULT_PAGINATION = False
+DEFAULT_DATE_FORMAT = '%Y %b %d %A'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+USE_FOLDER_AS_CATEGORY = False
+DEFAULT_CATEGORY = 'Articles'
+
+# DISPLAY_PAGES_ON_MENU = True
+# DISPLAY_CATEGORIES_ON_MENU = True
